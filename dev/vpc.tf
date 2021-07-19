856d92a77979b60807ccc25320870122c2e753d4
Geemac2021 <83748336+Geemac2021@users.noreply.github.com>
2021-07-19 04:23:40 -0500

    Update and rename main.tf to vpc.tf

--- dev/vpc.tf
+++ dev/vpc.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc23" {
-    cidr_block = "10.10.0.0.0/20"
+    cidr_block = "10.10.0.0/20"
     tags = {
         Name = "myvpc23"
         Environment = "dev"
@@ -15,11 +15,12 @@ resource "aws_vpc" "vpc23" {
 #ec2.tf
 resource "aws_instance" "server1" {
      ami =  "ami-0801628222e2e96d6"
-     instance_type = "t2 mirco"
+     instance_type = "t2.mirco"
      key_name      = "classkey24"
      tags = {
        Name  = "dbserver"
        Environment = "dev"
+  }      
 } 
 
 resource "aws_s3_bucket" "mys3" {
